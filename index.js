ab7bc12959f6c27c37719799ea0c301ab4d6301b
emon1995 <emonhossain995@gmail.com>
2023-04-30 21:50:24 +0600

    destination routes added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const destination = require("./data/destination.json");
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -8,6 +10,10 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/destination", (req, res) => {
+  res.send(destination);
 });
 
 app.listen(PORT, () => {
